ce4987c633780ba27c7ea40aa6c6f6079b95c86b
VoXaN24 <30030208+VoXaN24@users.noreply.github.com>
2021-11-01 21:57:30 +0100

    IPFS Script + Start autoinstall

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # send-ipfs-script
  
+Download file from internet and upload it automaticly to IPFS !
+
+Work thank's to the [Estuary Gateway](https://estuary.tech/)
+
+# Autoinstall script (Debian & Ubuntu Only)
+` | sudo bash`
---
+++ ipfs.sh
@@ -0,0 +1,57 @@
+#/bin/bash/
+echo "Copy file to IPFS"
+echo "Choose your source : 1 - Direct Link (wget) 2- Direct Link (Aria2c) 3- Gdrive Link (gdown), please enter the number of your choose"
+read choix
+
+if [ $choix == "2" ]; then
+    echo 'enter the url (under "")'
+    read url
+    echo 'enter the filename'
+    read filename
+    echo 'enter your estuary API Key'
+    read api
+    echo 'Start download'
+    aria2c -x16 $url --out="$filename"
+    myfilesize=$(wc -c "$filename" | awk '{print $1}')
+    echo 'Start upload'
+    cid=$(curl -X POST https://shuttle-1.estuary.tech/content/add -H "Authorization: Bearer $api" -H "Accept: application/json" -H "Content-Type: multipart/form-data" -F "data=@$filename" | jq -r '.cid')
+    echo "filename : $filename"
+    echo "size in byte : $myfilesize"
+    echo "IPFS CID: $cid"
+    rm -rf $filename
+elif [ $choix == "1" ]; then
+    echo 'enter the url (under "")'
+    read url
+    echo 'enter the filename'
+    read filename
+    echo 'enter your estuary API Key'
+    read api
+    echo 'Start download'
+    wget $url -O "$filename"
+    myfilesize=$(wc -c "$filename" | awk '{print $1}')
+    echo 'Start upload'
+    cid=$(curl -X POST https://shuttle-1.estuary.tech/content/add -H "Authorization: Bearer $api" -H "Accept: application/json" -H "Content-Type: multipart/form-data" -F "data=@$filename" | jq -r '.cid')
+    echo "filename : $filename"
+    echo "size in byte : $myfilesize"
+    echo "IPFS CID: $cid"
+    rm -rf $filename
+elif [ $choix == "3" ]; then
+    echo 'Enter the Gdrive ID File'
+    read idg
+    echo 'enter the filename'
+    read filename
+    echo 'enter your estuary API Key'
+    read api
+    echo 'Start download'
+    gdown https://drive.google.com/u/0/uc?id=$idg
+    echo 'Start upload'
+    cid=$(curl -X POST https://shuttle-1.estuary.tech/content/add -H "Authorization: Bearer $api" -H "Accept: application/json" -H "Content-Type: multipart/form-data" -F "data=@$filename" | jq -r '.cid')
+    myfilesize=$(wc -c "$filename" | awk '{print $1}')
+    echo "filename : $filename"
+    echo "size in byte : $myfilesize"
+    echo "IPFS CID: $cid"
+    rm -rf $filename
+else
+    echo 'Please make a choose next time'
+fi
+
