3cc4d28a5cad24686fea19a225e042359fb05c4f
VoXaN24 <30030208+VoXaN24@users.noreply.github.com>
2021-11-03 00:21:49 +0100

    Add local file support

--- ipfs.sh
+++ ipfs.sh
@@ -1,6 +1,6 @@
 #/bin/bash/
 echo "Copy file to IPFS"
-echo "Choose your source : 1 - Direct Link (wget) 2- Direct Link (Aria2c) 3- Gdrive Link (gdown), please enter the number of your choose"
+echo "Choose your source : 1 - Direct Link (wget) 2- Direct Link (Aria2c) 3- Gdrive Link (gdown) 4- A Local file, please enter the number of your choose"
 read choix
 
 if [ $choix == "2" ]; then
@@ -63,6 +63,18 @@ elif [ $choix == "3" ]; then
     echo "IPFS Download Link : https://dweb.link/ipfs/$cid"
     cd ..
     rm -rf ~/dl
+elif [ $choix == 4 ]; then
+    echo 'Enter THE FULL PATH of the file'
+    read path
+    echo 'enter your estuary API Key'
+    read api
+    cid=$(curl -X POST https://shuttle-1.estuary.tech/content/add -H "Authorization: Bearer $api" -H "Accept: application/json" -H "Content-Type: multipart/form-data" -F "data=@$path" | jq -r '.cid')
+    filename=$(printf '%b' "${path//%/\\x}")
+    myfilesize=$(wc -c "$path" | awk '{print $1}')
+    echo "filename : $filename"
+    echo "size in byte : $myfilesize"
+    echo "IPFS CID: /ipfs/$cid"
+    echo "IPFS Download Link : https://dweb.link/ipfs/$cid"
 else
     echo 'Please make a choose next time'
 fi
